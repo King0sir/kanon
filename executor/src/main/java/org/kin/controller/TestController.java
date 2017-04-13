be896a77133defa7b3e07935a3d2b230716bf247
kin <kinminghao.king.sir@gmail.com>
2017-04-14 00:01:51 +0800

    tester

--- executor/src/main/java/org/kin/controller/TestController.java
+++ executor/src/main/java/org/kin/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public String index(){
         return "Kanon is running";
     }
