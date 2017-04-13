25e7396942e33a58e78708f626663643386251d3
kin <kinminghao.king.sir@gmail.com>
2017-04-13 23:55:20 +0800

    test controller

---
+++ executor/src/main/java/org/kin/controller/TestController.java
@@ -0,0 +1,17 @@
+package org.kin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by kinakihiro on 2017/4/13.
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Kanon is running";
+    }
+
+}
